e8cfe089d0738978e35b678752b9559ddd654f92
wlupus <arthurreload@outlook.com>
2021-11-22 00:58:41 +0800

    update help message, add /pneuma cmd

--- src/command/help.go
+++ src/command/help.go
@@ -10,18 +10,21 @@ const (
 /help show this help message
 /about about me
 /language change default language e.g. /language en
+/pneuma get the value of pneuma
 language list: en, cn, jp
 `
     helpTextJP string = `コマンド リスト：
 /help ヘルプメッセージ
 /about 私のこと
 /language change default language e.g. /language en
+/pneuma get the value of pneuma
 language リスト: en, cn, jp
 `
     helpTextCN string = `指令列表: 
 /help 显示本条帮助信息
 /about 关于
 /language 更改默认语言 例如: /language cn
+/pneuma 查询pneuma余量
 语言可选值: en, cn, jp
 `
 )
